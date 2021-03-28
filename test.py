53fe3ae4d31af251483ccee7a4565a8c3f8c3928
Marvis <marvisn@uci.edu>
2021-03-27 20:01:31 -0700

    Simple Gestures

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import mediapipe as mp
 import cv2
+import SimpleGestureDetector as gd
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 # From Mediapipe Hands Example
+#For webcam input:
 cap = cv2.VideoCapture(0)
+gesture = gd.SimpleGestureDetector()
 with mp_hands.Hands(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
@@ -27,8 +30,20 @@ with mp_hands.Hands(
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    width  = cap.get(3)  # float `width`
+    height = cap.get(4)  # float `height`
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
+        #print('hand_landmarks:', hand_landmarks)
+        # print(
+        #     f'Index finger tip coordinates: (',
+        #     f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * width}, '
+        #     f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * height})'
+        # )
+        
+        # Detect Gestures
+        gesture.simpleGesture(hand_landmarks.landmark)
+        
         mp_drawing.draw_landmarks(
             image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
     cv2.imshow('MediaPipe Hands', image)
